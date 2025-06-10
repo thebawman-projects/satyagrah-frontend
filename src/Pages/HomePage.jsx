ccc503fa7dd582ad3c56e1d2d720cae50bfe7a98
thebawman-projects <techyargha2018@gmail.com>
2025-06-10 07:04:40 +0530

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -29,7 +29,7 @@ const HomePage = () => {
       <div className="pt-[5.5rem]"> 
       <Slider />
       </div>
-      <ScholarshipBanner/>
+      <ScholarshipHero/>
       <CourseHome />
       <JSCounter />
       <RegistrationLinkCard />
